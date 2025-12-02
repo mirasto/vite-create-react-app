a31eb12d79521fa1d696f49d12a44d3fe0a336e1
mirasto <76472298+mirasto@users.noreply.github.com>
2025-12-02 17:47:53 +0200

    Change base path and add path aliases in config
    Updated the base path for the project and added path aliases for easier imports.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+//import tailwindcss from '@tailwindcss/vite';
+import path from 'path'; 
 
-const BASE_PATH = '/vite-create-react-app/';
+const BASE_PATH = '/repository-name/';
+
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? BASE_PATH : '/',
   plugins: [
@@ -10,5 +13,15 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+  //  tailwindcss(),
   ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@service': path.resolve(__dirname, './src/service'),
+    },
+  },
 });

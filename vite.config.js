ca9aa48dbfeae91e8385afbb5aa7a6eca9a0ca9c
mirasto <76472298+mirasto@users.noreply.github.com>
2025-12-18 14:17:44 +0200

    Refactor Vite config for dynamic base and plugins
    Refactor Vite configuration to use dynamic base path and plugins.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-//import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+// import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const BASE_PATH = '/repository-name/';
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? BASE_PATH : '/',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    //  tailwindcss(),
-  ],
-
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? BASE_PATH : '/',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@service': path.resolve(__dirname, './src/service'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@axiosConfig': path.resolve(__dirname, './src/axiosConfig'),
     },
   },
-});
+}));
